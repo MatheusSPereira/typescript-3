2b082208cc08538b5fb903dcae4361f6c25e0964
MatheusSPereira <githubmatheus@outlook.com>
2023-08-01 08:22:38 -0300

    first commit

---
+++ app/src/app.ts
@@ -0,0 +1,21 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form){
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error("Nao foi possievl inicializar sua aplicação.")
+}
+
+const botaoImporta = document.querySelector('#botao-importa');
+if(botaoImporta){
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados();
+    });
+} else {
+    throw Error("Botao importa nao foi encontrado");
+}
